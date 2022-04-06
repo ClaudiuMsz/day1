26bc72b8afba2945852fc7db75d038cf126f2da3
ClaudiuMsz <103144030+ClaudiuMsz@users.noreply.github.com>
2022-04-06 23:42:31 +0300

    Initial commit

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+
+// import base config
+const webpackConfigBase = require('./webpack.config.base');
+
+module.exports = merge(webpackConfigBase, {
+  mode: 'development',
+  output: {
+    filename: '[name].js',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
